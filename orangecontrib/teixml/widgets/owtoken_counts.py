33169dd32f017ddf8072a6b85f9055596587bd5f
Your Name <rubygems@chrislee.dhs.org>
2025-06-23 23:55:14 -0400

    added checkboxes to select attributes to use for token/types

--- orangecontrib/teixml/widgets/owtoken_counts.py
+++ orangecontrib/teixml/widgets/owtoken_counts.py
@@ -4,7 +4,7 @@ from collections import Counter, defaultdict
 
 import numpy as np
 
-from AnyQt.QtWidgets import QFileDialog
+from AnyQt.QtWidgets import QFileDialog, QCheckBox
 from AnyQt.QtCore import Qt, QThread, pyqtSignal
 
 from Orange.widgets import gui
@@ -17,11 +17,12 @@ class TokenExtractorWorker(QThread):
     result = pyqtSignal(Table)  # emits the extract tokens Table
     progress = pyqtSignal(int)  # emits progress (0-100)
 
-    def __init__(self, directory: str, top_n: int, normalize: bool):
+    def __init__(self, directory: str, top_n: int, normalize: bool, attr_names: list[str]):
         super().__init__()
         self.directory = directory
         self.top_n = top_n
         self.normalize = normalize
+        self.attr_names = attr_names
 
     def run(self):
         directory = self.directory
@@ -45,10 +46,17 @@ class TokenExtractorWorker(QThread):
                 file_counter = Counter()
                 for elem in root.iter():
                     if elem.tag.endswith("w"):
-                        lemma = elem.attrib.get("lemma")
-                        pos = elem.attrib.get("ana") or elem.attrib.get("pos")
-                        if lemma and pos:
-                            file_counter[(lemma, pos)] += 1
+                        attributes = []
+                        for attr_name in self.attr_names:
+                            if attr_name == "word":
+                                attributes.append(elem.text)
+                            else:
+                                val = elem.attrib.get(attr_name, "")
+                                val = val.strip("#")
+                                attributes.append(val)
+                        attributes = tuple(attributes)
+                        if not all(x == "" for x in attributes):
+                            file_counter[attributes] += 1
 
                 counters.append(file_counter)
                 all_tokens.update(file_counter)
@@ -58,8 +66,9 @@ class TokenExtractorWorker(QThread):
             set_progress((100 * (i + 1)) // len(files))
 
         top_tokens = [tok for tok, _ in all_tokens.most_common(top_n)]
-        variables = [ContinuousVariable("Total Tokens"), ContinuousVariable("Uniq Tokens")] + \
-            [ContinuousVariable(f"{lemma}[{pos}]") for lemma, pos in top_tokens]
+        print(top_tokens)
+        variables = [ContinuousVariable("Total Words"), ContinuousVariable("Total Types")] + \
+            [ContinuousVariable("_".join(token)) for token in top_tokens]
         domain = Domain(attributes=variables, class_vars=[], metas=[StringVariable("title")])
 
         rows = []
@@ -99,6 +108,17 @@ class OWTEITokenExtractor(OWWidget, ConcurrentMixin):
     directory = Setting("")
     top_n = Setting(100)
     normalize = Setting(False)
+    attr_names = Setting(["word", "lemma", "pos", "reg", "ana"])
+
+    ATTRIBUTE_NAMES = [
+        ("word", "Original Word"),
+        ("lemma", "Lemma"),
+        ("pos", "Part of Speech"),
+        ("reg", "Regularization"),
+        ("ana", "Annotation")
+    ]
+    ATTRIBUTE_ORDER = {name: i for i, (name, _) in enumerate(ATTRIBUTE_NAMES)}
+
 
     def __init__(self):
         super().__init__()
@@ -118,6 +138,19 @@ class OWTEITokenExtractor(OWWidget, ConcurrentMixin):
         self.normalize_checkbox = gui.checkBox(box, self, "normalize", "Normalize")
         self.normalize_checkbox.setToolTip("Divide the counts by the total number of tokens per document.")
         self.normalize_checkbox.clicked.connect(self.on_normalize_checked)
+        # Checkboxes for token attributes
+        self.token_attr_box = gui.vBox(box, box="Token Attributes")
+        self.checkbox_map = {}
+
+        # Create checkboxes and connect state change handlers
+        for attr_name, label in OWTEITokenExtractor.ATTRIBUTE_NAMES:
+            checkbox = QCheckBox(label)
+            if attr_name in self.attr_names:
+                checkbox.setChecked(True)
+            checkbox.stateChanged.connect(self.on_checkbox_state_changed(attr_name))
+            self.token_attr_box.layout().addWidget(checkbox)
+            self.checkbox_map[attr_name] = checkbox
+            
         self.extract_button = gui.button(box, self, "Extract Tokens", callback=self.start_extraction)
         self.mainArea.layout().setAlignment(Qt.AlignTop)
 
@@ -141,13 +174,27 @@ class OWTEITokenExtractor(OWWidget, ConcurrentMixin):
             new_table = Table(table.domain, X_df.to_numpy(), table.Y, table.metas)
             self.Outputs.data.send(new_table)
 
+    def on_checkbox_state_changed(self, attr_name):
+        def handler(state):
+            if state:
+                if attr_name not in self.attr_names:
+                    self.attr_names.append(attr_name)
+            else:
+                if attr_name in self.attr_names:
+                    self.attr_names.remove(attr_name)
+        return handler
+
     def start_extraction(self):
         if not self.directory:
             self.error("Please choose a directory.")
             return
         self.error()
         self.progressBarInit()
-        self.worker = TokenExtractorWorker(self.directory, int(self.top_n), self.normalize)
+        self.attr_names = sorted(
+            self.attr_names, 
+            key = lambda name: OWTEITokenExtractor.ATTRIBUTE_ORDER.get(name, float('inf'))
+        )
+        self.worker = TokenExtractorWorker(self.directory, int(self.top_n), self.normalize, self.attr_names)
         self.worker.progress.connect(self.progressBarSet)
         self.worker.result.connect(self._on_result)
         self.worker.start()
@@ -162,6 +209,6 @@ if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview
     w = WidgetPreview(OWTEITokenExtractor)
     w.create_widget()
-    w.widget.directory = "/home/chris/Downloads/shakespeares-works_TEIsimple_FolgerShakespeare"
+    w.widget.directory = "tests/inputs"
     w.run()
     
